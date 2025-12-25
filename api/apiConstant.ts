feb563080364350c4ca8e8c32f43ee345205badd
MAC <mac@MacBook-Pro.local>
2025-12-25 22:57:27 +0700

    [update blog]

--- api/apiConstant.ts
+++ api/apiConstant.ts
@@ -1,6 +1,6 @@
 const ApiConstant = {
-  BASE_API_URL: `http://localhost:4000/api`,
-  // BASE_API_URL: `https://thotool.com/api`
+  // BASE_API_URL: `http://localhost:4000/api`,
+  BASE_API_URL: `https://thotool.com/api`
 };
 
 export default ApiConstant;
