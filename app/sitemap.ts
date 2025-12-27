b735b29f7df1dbb9d46a8d0e98c9c1bb6ba65d95
MAC <mac@MacBookPro.lan>
2025-12-27 16:09:38 +0700

    slugs

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,11 +7,23 @@ const BLOG_SLUGS = [
   'cach-lay-id-bai-viet-facebook',
   'lay-thong-tin-bai-viet-facebook',
   'tool-cao-comment-facebook',
+  'tool-cao-comment-facebook',
+]
+const FB_SLUGS = [
+  'find-comment-post',
+  'find-post-info'
 ]
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const blogUrls = BLOG_SLUGS.map((slug) => ({
     url: `${SITE_URL}/blog/${slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'weekly' as const,
+    priority: 0.8,
+  }))
+
+  const toolFbUrls = FB_SLUGS.map((slug) => ({
+    url: `${SITE_URL}/facebook/${slug}`,
     lastModified: new Date(),
     changeFrequency: 'weekly' as const,
     priority: 0.8,
@@ -30,5 +42,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.7,
     },
     ...blogUrls,
+    ...toolFbUrls
   ]
 }
